ae9411dceb831966d31d3bf667d4ccd36a1214d4
Cedric Verstraeten <cedric@verstraeten.io>
2023-04-12 21:18:26 +0200

    first version of using openalpr with go

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/kerberos-io/openalpr
+
+go 1.19
+
+require github.com/openalpr/openalpr v2.3.0+incompatible
---
+++ main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+
+	"github.com/openalpr/openalpr/src/bindings/go/openalpr"
+)
+
+func main() {
+	alpr := openalpr.NewAlpr("eu", "", "/var/lib/openalpr/runtime_data")
+	defer alpr.Unload()
+
+	if !alpr.IsLoaded() {
+		fmt.Println("OpenAlpr failed to load!")
+		return
+	}
+	alpr.SetTopN(20)
+
+	fmt.Println(alpr.IsLoaded())
+	fmt.Println(openalpr.GetVersion())
+
+	resultFromFilePath, err := alpr.RecognizeByFilePath("./car.png")
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Printf("%+v\n", resultFromFilePath)
+	fmt.Printf("\n\n\n")
+
+	imageBytes, err := ioutil.ReadFile("./car3.png")
+	if err != nil {
+		fmt.Println(err)
+	}
+	resultFromBlob, err := alpr.RecognizeByBlob(imageBytes)
+	fmt.Printf("%+v\n", resultFromBlob)
+}
